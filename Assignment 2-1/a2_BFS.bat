8129c60b7b7cc4c4006966c8a4c6c9a3bd7a68cb
rahul <rahulraju3107@gmail.com>
2025-04-09 11:04:58 +1000

    Add files via upload

---
+++ Assignment 2-1/a2_BFS.bat
@@ -0,0 +1,6 @@
+@echo off
+
+set filename=%~1
+set method=%~2
+
+python a2_BFS.py %filename% %method%
---
+++ Assignment 2-1/a2_DFS.bat
@@ -0,0 +1,6 @@
+@echo off
+
+set filename=%~1
+set method=%~2
+
+python a2_DFS.py %filename% %method%
