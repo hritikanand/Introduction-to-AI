fb03e98811492e9522a6435e4d549682cb848c22
charlotte-f24 <charlottefederico8@gmail.com>
2025-04-11 23:48:02 +1000

    Add files via upload

---
+++ Assignment 2-1/test.bat
@@ -0,0 +1,85 @@
+@echo off
+rem Batch file to run search algorithms with different test cases in the test_cases folder
+
+rem Test 1
+python searches.py test_cases\test1.txt DFS
+python searches.py test_cases\test1.txt BFS
+python searches.py test_cases\test1.txt GBFS
+python searches.py test_cases\test1.txt Astar
+python searches.py test_cases\test1.txt CUS1
+python searches.py test_cases\test1.txt CUS2
+
+rem Test 2
+python searches.py test_cases\test2.txt DFS
+python searches.py test_cases\test2.txt BFS
+python searches.py test_cases\test2.txt GBFS
+python searches.py test_cases\test2.txt Astar
+python searches.py test_cases\test2.txt CUS1
+python searches.py test_cases\test2.txt CUS2
+
+rem Test 3
+python searches.py test_cases\test3.txt DFS
+python searches.py test_cases\test3.txt BFS
+python searches.py test_cases\test3.txt GBFS
+python searches.py test_cases\test3.txt Astar
+python searches.py test_cases\test3.txt CUS1
+python searches.py test_cases\test3.txt CUS2
+
+rem Test 4
+python searches.py test_cases\test4.txt DFS
+python searches.py test_cases\test4.txt BFS
+python searches.py test_cases\test4.txt GBFS
+python searches.py test_cases\test4.txt Astar
+python searches.py test_cases\test4.txt CUS1
+python searches.py test_cases\test4.txt CUS2
+
+rem Test 5
+python searches.py test_cases\test5.txt DFS
+python searches.py test_cases\test5.txt BFS
+python searches.py test_cases\test5.txt GBFS
+python searches.py test_cases\test5.txt Astar
+python searches.py test_cases\test5.txt CUS1
+python searches.py test_cases\test5.txt CUS2
+
+rem Test 6
+python searches.py test_cases\test6.txt DFS
+python searches.py test_cases\test6.txt BFS
+python searches.py test_cases\test6.txt GBFS
+python searches.py test_cases\test6.txt Astar
+python searches.py test_cases\test6.txt CUS1
+python searches.py test_cases\test6.txt CUS2
+
+rem Test 7
+python searches.py test_cases\test7.txt DFS
+python searches.py test_cases\test7.txt BFS
+python searches.py test_cases\test7.txt GBFS
+python searches.py test_cases\test7.txt Astar
+python searches.py test_cases\test7.txt CUS1
+python searches.py test_cases\test7.txt CUS2
+
+rem Test 8
+python searches.py test_cases\test8.txt DFS
+python searches.py test_cases\test8.txt BFS
+python searches.py test_cases\test8.txt GBFS
+python searches.py test_cases\test8.txt Astar
+python searches.py test_cases\test8.txt CUS1
+python searches.py test_cases\test8.txt CUS2
+
+rem Test 9
+python searches.py test_cases\test9.txt DFS
+python searches.py test_cases\test9.txt BFS
+python searches.py test_cases\test9.txt GBFS
+python searches.py test_cases\test9.txt Astar
+python searches.py test_cases\test9.txt CUS1
+python searches.py test_cases\test9.txt CUS2
+
+rem Test 10
+python searches.py test_cases\test10.txt DFS
+python searches.py test_cases\test10.txt BFS
+python searches.py test_cases\test10.txt GBFS
+python searches.py test_cases\test10.txt Astar
+python searches.py test_cases\test10.txt CUS1
+python searches.py test_cases\test10.txt CUS2
+
+echo All tests completed.
+pause
